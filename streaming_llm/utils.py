715f2df754000d23809b28af26b6d1487034c62d
Giannis Chatziveroglou <giannischatziveroglou@gmail.com>
2024-11-30 12:08:01 -0500

    nit

--- streaming_llm/utils.py
+++ streaming_llm/utils.py
@@ -52,7 +52,7 @@ def parse_args():
 def embed_text(text, model, tokenizer):
     tokens = tokenizer(text, return_tensors="pt").input_ids.to("cuda")
     model_output = model(input_ids=tokens, output_hidden_states=True)
-    embeddings = model_output.hidden_states[-1].mean(dim=1)[0].unsqueeze(0).item()
+    embeddings = model_output.hidden_states[-1].mean(dim=1)[0].unsqueeze(0).cpu()
     norm_embeddings = embeddings / torch.norm(embeddings, p=2)
     return norm_embeddings
 
