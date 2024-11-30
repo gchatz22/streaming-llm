31042b4ceab68db6b3d084114996d6e527693a91
Giannis Chatziveroglou <giannischatziveroglou@gmail.com>
2024-11-30 11:51:58 -0500

    nit

--- examples/run_streaming_llama.py
+++ examples/run_streaming_llama.py
@@ -115,7 +115,10 @@ def streaming_inference(
             )
 
         if past_key_values is not None:
-            past_key_values = past_key_values.to("cuda")
+            past_key_values = (
+                past_key_values[0].to("cuda"),
+                past_key_values[1].to("cuda"),
+            )
         past_key_values, generated_ids = greedy_generate(
             model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
         )
